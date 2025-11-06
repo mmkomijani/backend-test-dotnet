40a0416574e1e757b6df1e6ad91c264965682b8b
mmkomijani <mmkomijani@gmail.com>
2025-11-06 12:11:47 +0330

    Add files via upload
    Added CRUD API project

---
+++ GoldenCityApiTest/Controllers/NotesController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GoldenCityApiTest.Controllers
+{
+    [Route("notes")]
+    [ApiController]
+    public class NotesController : ControllerBase
+    {
+        private static List<Note> notes = new List<Note>();
+        private static int nextid = 1;
+
+        //– Create a new note
+        [HttpPost]
+        public IActionResult Create(Note note)
+        {
+            note.Id = nextid++;
+            notes.Add(note);
+            return CreatedAtAction(nameof(GetById), new { id = note.Id }, note);
+
+        }
+
+        //– Retrieve all notes
+        [HttpGet]
+        public IActionResult GetAll() => Ok(notes);
+
+        //– Retrieve a specific note by ID
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var note = notes.FirstOrDefault(n => n.Id == id);
+            if (note == null) return NotFound();
+            return Ok(note);
+
+        }
+
+        // – Update a note by ID
+        [HttpPut("{id}")]
+        public IActionResult Update(int id,Note UpdateNote)
+        {
+            var note = notes.FirstOrDefault(n => n.Id == id);
+            if (note==null)return NotFound();
+            note.Name = UpdateNote.Name;
+            note.Description = UpdateNote.Description;
+            return Ok(note);
+
+
+        }
+
+        //– Delete a note by ID
+        [HttpDelete("{id}")]
+        public IActionResult DeleteById(int id) 
+        {
+            var note = notes.FirstOrDefault(n => n.Id == id);
+                if(note==null) return NotFound();
+
+                notes.Remove(note);
+                return NoContent();
+        }
+
+    }
+}
---
+++ GoldenCityApiTest/Note.cs
@@ -0,0 +1,15 @@
+﻿namespace GoldenCityApiTest
+{
+    public class Note
+    {
+        // numbet of Note
+        public int Id { get; set; }
+
+        // title of Note
+        public string Name { get; set; }
+
+        // description of Note
+        public string Description { get; set; }
+
+    }
+}
